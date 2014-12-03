5cf6e70fd7910e6a763ca21d51a6514feafc17fd
luis rodriguez <luis.rodriguez@cru.org>
2014-12-03 14:51:06 -0500

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'aem_disp_config'
+maintainer       'cru'
+maintainer_email 'luis.rodriguez@cru.org'
+maintainer_email 'luis.rodriguez'
+license          'All rights reserved'
+description      'Installs/Configures aem_disp_config'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
+version          '0.1.2'
