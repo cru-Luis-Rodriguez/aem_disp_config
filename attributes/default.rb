bd3ceb8851c928181fafc193bc323f3d7a118259
luis rodriguez <luis.rodriguez@cru.org>
2014-12-04 17:17:04 -0500

    remove the @ symbol form attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 # General settings
-default['aem']['apache']['home']="/etc/apache2"
-default['dispatcher']['site']="some_site"
-default['dispatcher']['alias1']="some_site_alias"
-default['dispatcher']['alias2']="some_site_alias
-default['vhost']['email']="some_email@domain"
+default['aem']['apache']['home'] = "/etc/apache2"
+default['dispatcher']['site'] = "some_site"
+default['dispatcher']['alias1'] = "some_site_alias"
+default['dispatcher']['alias2'] = "some_site_alias
+default['vhost']['email'] = "some_email"
