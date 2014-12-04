74091f57295a3f9c2e8053ad06c23e20e4b1cc79
luis rodriguez <luis.rodriguez@cru.org>
2014-12-04 11:21:54 -0500

    modifies testing to include enviroment json

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,5 @@ description      'Installs/Configures aem_disp_config'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 version          '0.1.2'
+
+depends "apache2"
