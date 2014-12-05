b364838c36b5154ef03c83233524bed007e33e03
luis rodriguez <luis.rodriguez@cru.org>
2014-12-05 15:12:26 -0500

    fix issue with metadata fle

--- metadata.rb
+++ metadata.rb
@@ -8,4 +8,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 version          '0.1.2'
 
-depends "apache2", "=> 3.0.0"
+depends "apache2", "3.0.0"
