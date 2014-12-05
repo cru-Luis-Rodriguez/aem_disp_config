ceedb7503571c34784027f7adcf78d22bc55af2b
luis rodriguez <luis.rodriguez@cru.org>
2014-12-05 16:03:53 -0500

    modified metadata to depend on apt

--- metadata.rb
+++ metadata.rb
@@ -8,4 +8,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 version          '0.1.2'
 
+depends "apt"
 depends "apache2", "3.0.0"
